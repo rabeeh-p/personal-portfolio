795ef9f3ca376dfc8b6f57185e9a90649c8b0ef9
rabeeh <rabeehpuzhakkalakath8547@gmail.com>
2024-05-22 12:49:24 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function sendMail(){
+    let data={
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject: document.getElementById("subject").value,
+        message: document.getElementById("message").value
+    }
+
+     // Check if any field is empty
+     if (!data.name) {
+        alert("Name field is empty");
+        return; // Stop the function execution if this field is empty
+    }
+    if (!data.email) {
+        alert("Email field is empty");
+        return; // Stop the function execution if this field is empty
+    }
+    if (!data.subject) {
+        alert("Subject field is empty");
+        return; // Stop the function execution if this field is empty
+    }
+    if (!data.message) {
+        alert("Message field is empty");
+        return; // Stop the function execution if this field is empty
+    }
+    emailjs.send("service_oqq2179","template_moso2hz",data).then(alert("Email sent!"))
+}
